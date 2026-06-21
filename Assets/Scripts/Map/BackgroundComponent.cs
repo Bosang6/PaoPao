b7a2388b70a8ccc006116380ae0c075e3be633f9
Bosang6 <huangzhikang66@gmail.com>
2026-06-21 14:49:12 +0200

    Add Background component to Map prefab

---
+++ Assets/Scripts/Map/BackgroundComponent.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class BackgroundComponent : MonoBehaviour
+{
+    void Start()
+    {
+        Fit();
+    }
+    
+    private void Fit()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        Camera cam = Camera.main;
+
+        float cameraHeight = cam.orthographicSize * 2f;
+        float cameraWidth = cameraHeight * cam.aspect;
+
+        Vector2 spriteSize = sr.sprite.bounds.size;
+
+        float scaleX = cameraWidth / spriteSize.x;
+        float scaleY = cameraHeight / spriteSize.y;
+
+        float scale = Mathf.Max(scaleX, scaleY);
+
+        transform.localScale = new Vector3(scale, scale, 1f);
+    }
+}
